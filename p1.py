2a9c0e98d54d0fae522c5d29dfbcdcb83c75d448
computerman2027 <163759052+computerman2027@users.noreply.github.com>
2024-11-09 10:29:56 +0530

    commit 1

---
+++ p1.py
@@ -0,0 +1,19 @@
+import time
+
+def fib(n:int) -> int:
+    if(n<=1):
+        return n
+    else:
+        return fib(n-1)+fib(n-2)
+
+n=int(input("Enter Range : "))
+
+start_time = time.time()
+
+for i in range(0,n):
+    print(f'{i}: {fib(i)}')
+
+end_time = time.time()
+
+elapsed_time = end_time - start_time
+print(f"\nTime taken: {elapsed_time:.4f} seconds")
